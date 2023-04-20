c268205abce6cb5464ef67e44361f6c9e18d1dfa
Tomás De Donatis <tomasdedonatis@hotmail.com>
2023-04-19 23:43:04 -0300

    Control Jugador Terminado

---
+++ Assets/Otros/DV/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float runSpeed = 16f;
+    public float crouchSpeed = 6f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if(Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            controller.Move(move * runSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            controller.Move(move * crouchSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}

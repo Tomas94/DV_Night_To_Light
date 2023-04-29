a30d087d20e9dc226d567dd818bc02438b8e8a4b
Tomás De Donatis <tomasdedonatis@hotmail.com>
2023-04-29 10:47:54 -0300

    Escalado Modelos

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -19,7 +19,7 @@ public class MouseLook : MonoBehaviour
         float MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= MouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * MouseX);

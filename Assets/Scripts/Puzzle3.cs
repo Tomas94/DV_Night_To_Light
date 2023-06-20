3bd3ffe596be100579e940eb0edd22dd390e84ad
Tomás De Donatis <tomasdedonatis@hotmail.com>
2023-06-20 04:11:01 -0300

    Update iglesia

---
+++ Assets/Scripts/Puzzle3.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3 : MonoBehaviour
+{
+    int puntos;
+    public GameObject[] botonescorrectos;
+    public GameObject[] botonesIncorrectos;
+    int contador;
+
+    void Start()
+    {
+        contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+
+
+
+}
